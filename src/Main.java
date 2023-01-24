b24e655e7a096376d98eb36a995fd06d0011432d
L1Gross <profgross@gmail.com>
2023-01-24 12:11:06 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+/**
+ * This is the Main class in the Java program.
+ */
+public class Main {
+    /*
+     The class header: public class Main
+     */
+
+    /**
+     * This is the main method in the Java program.
+     * @param args
+     */
+    public static void main(String[] args) {
+        /*
+         The method header: public static void main(String[] args)
+         A static method is like a function in Python.
+         Note the syntax for a multi-line comment.
+         */
+        // Note print does not advance to the next line for subsequent printing.
+        System.out.print("Hello world!");
+        System.out.println("\tHello world!"); // Note println contains your newline.
+        System.out.print("\tHello 'Tatooine'!\n");
+        System.out.print("\n");
+        System.out.println("Hello\\Goodbye \"Alderan\"!");
+    }
+}
